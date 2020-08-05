f8b221d1a30742e64aba3543b3f9bc25cc83d5f8
unknown <qwop1606@gmail.com>
2020-08-05 20:34:53 +0300

    Компоненты: rowCard, blockCard, favoriteBtn

--- src/components/FavoriteBtn.jsx
+++ src/components/FavoriteBtn.jsx
@@ -1,1 +1,11 @@
 // Крошечный компонент для удобного внедрения и во славу чистому коду
+import React from 'react';
+
+const favoriteBtn = (props) => {
+    return(
+        <div className='favoriteBtn'>
+            Добавить в избранное
+        </div>
+    )
+}
+export default favoriteBtn;
--- src/components/RowCard.jsx
+++ src/components/RowCard.jsx
@@ -1,1 +1,20 @@
 // Строчный вид представления карточки (на многих интернет магазинах есть примеры)
+
+const rowCard = () => {
+    return (
+        <div className='rowCard'>
+            <img src='' alt='Фото блюда'></img>
+            <div className='rowCard_info'>
+                <h3 className='rowCard_info--title'></h3>
+                <div className='rowCard_time--preparing'>
+                    {/* Время приготовления */}
+                    <span></span>
+                    <span></span>
+                </div>
+                <span className='rowCard_complexity--recipe'>
+                    {/* Сложность рецепта */}
+                </span>
+            </div>
+        </div>
+    )
+}
